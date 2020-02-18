f5bd60e345c9faedb93c723760c2b8faccb9def2
chao wang <wc42888@hotmail.com>
2020-02-19 09:10:43 +1100

    added config for api requests

---
+++ src/config/api.js
@@ -0,0 +1,9 @@
+// TODO: hard coded public and private key, will need to move to
+//  .env file for security reason (can be injected in to the code
+//  react-native-config)
+
+export default {
+  publicKey: '2a5e033cd89f28d229ce8b3560d6d176',
+  privateKey: '42ab4191ab09b9f148ca5b2f16dfbca42e48a9e4',
+  baseUrl: 'https://gateway.marvel.com/v1/public',
+};
