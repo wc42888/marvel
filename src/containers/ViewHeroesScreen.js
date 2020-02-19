ac0bf2e476e33a756ab50066411af486f4f461df
chao wang <wc42888@hotmail.com>
2020-02-19 21:04:33 +1100

    added new components to the screen

--- src/containers/ViewHeroesScreen.js
+++ src/containers/ViewHeroesScreen.js
@@ -1,23 +1,53 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {connect} from 'react-redux';
-import {View, Text} from 'react-native';
+import {View, StyleSheet} from 'react-native';
 import PropTypes from 'prop-types';
 import {getMarvelHero} from '../redux/actions/marvelHeros';
-import {marvelHeroesSelect} from '../selectors';
+import {SearchBar, HeroList} from '../components/ViewHeroesScreen';
+import {marvelHeroesSelect, loadingSelect} from '../selectors';
 
-const ViewHeroesScreen = ({getMarvelHero, allMarvelHeroes}) => {
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+});
+
+const ViewHeroesScreen = ({
+  getMarvelHero,
+  isRetrivingHeroes,
+  allMarvelHeroes,
+}) => {
+  const [searchText, setSearchText] = useState(null);
+
+  const clearSearchText = () => setSearchText(null);
+
   useEffect(() => {
-    (async function anyNameFunction() {
+    const fetchHeroes = async () => {
       await getMarvelHero();
-    })();
+    };
 
+    fetchHeroes();
     //  disable the role because the fetch function only runs once
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const {root} = styles;
+
   return (
-    <View>
-      <Text>hello world</Text>
+    <View style={root}>
+      <SearchBar
+        clearSearchText={clearSearchText}
+        searchText={searchText}
+        setSearchText={setSearchText}
+      />
+      <HeroList
+        searchText={searchText}
+        allHeroes={allMarvelHeroes}
+        isRetrivingHeroes={isRetrivingHeroes}
+        getMarvelHero={getMarvelHero}
+      />
     </View>
   );
 };
@@ -28,6 +58,7 @@ ViewHeroesScreen.propTypes = {
 
 const mapStateToProps = state => ({
   allMarvelHeroes: marvelHeroesSelect.selectAllMarvelHeroes(state),
+  isRetrivingHeroes: loadingSelect.isRetrivingHeroes(state),
 });
 
 export default connect(
