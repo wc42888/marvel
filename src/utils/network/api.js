d939af501dc27c61ddf8f8e8d7b6bd3543e89b82
chao wang <wc42888@hotmail.com>
2020-02-20 21:05:16 +1100

    refactored api

--- src/utils/network/api.js
+++ src/utils/network/api.js
@@ -2,20 +2,26 @@ import {MD5} from 'react-native-crypto-js';
 import HTTP from './Http';
 import apiConfig from '../../config/api';
 
-const {privateKey, publicKey, baseUrl} = apiConfig;
+const {privateKey, publicKey} = apiConfig;
 
 // more specific http request configuartion (auth, timeouts etc) can be added
-const marvelRequests = new HTTP({
-  baseURL: baseUrl,
-});
+const marvelRequests = new HTTP();
 
 const generateHash = timestamp => MD5(`${timestamp}${privateKey}${publicKey}`);
 
-const getMarvelData = async (url, config) => marvelRequests.get(url, config);
-
-export const getAllHeros = async () => {
+const getParams = () => {
   const timestamp = +new Date();
   const hash = generateHash(timestamp);
   const params = `apikey=${publicKey}&ts=${timestamp}&hash=${hash}`;
-  return getMarvelData(`/characters?${params}`);
+  return params;
 };
+
+const getMarvelData = async (url, config) => marvelRequests.get(url, config);
+
+export const getAllHeros = async () =>
+  getMarvelData(
+    `https://gateway.marvel.com/v1/public/characters?${getParams()}`,
+  );
+
+export const getHeroComics = async url =>
+  getMarvelData(`${url}?${getParams()}`);
